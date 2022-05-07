307f8974e5394cd55c0e9e0cb26314af8c654bc9
Pat Kearns <pat.kearns@dbtlabs.com>
2022-05-07 03:06:17 +0000

    fun with variables

--- dbt_snowflake/models/demos/global_variables/variable_usage.sql
+++ dbt_snowflake/models/demos/global_variables/variable_usage.sql
@@ -1,5 +1,18 @@
 {% set local_var="'my_quoted_var'" %}
 
 select 
-    '{{var('my_cool_var')}}'::date as col_a,
-    {{local_var}} as col_b
+    '{{ var('my_cool_var') }}'::date as col_a,
+    {{ local_var }} as col_b,
+    {{ var('disable_models') }} as disable_models,
+    
+    {%- if "some_model_name" in var('disable_models') %}
+        true 
+        {%- else %}
+        false
+    {%- endif %} 
+    as some_flag,
+    '{{ this.name }}' as this__name,
+
+{% set disable_models="['some_other_model_name']" %}
+
+    {{ disable_models }} as disable_models_updated
