a86e0ef7bad82546b9eb9700692581f40ae5507e
Pat Kearns <pat.kearns@dbtlabs.com>
2022-08-06 02:32:29 +0000

    fun with variables and graph objects

--- dbt_snowflake/models/marts/core/orders.sql
+++ dbt_snowflake/models/marts/core/orders.sql
@@ -29,7 +29,6 @@ final as (
         orders.customer_id,
         orders.order_date,
         coalesce(order_payments.amount_usd, 0) as amount
-
     from orders
     left join order_payments using (order_id)
 )
