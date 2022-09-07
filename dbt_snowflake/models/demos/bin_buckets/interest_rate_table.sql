1b07c930325b0610580d6195cc173dea7480a34a
Pat Kearns <pat.kearns@dbtlabs.com>
2022-09-07 01:40:19 +0000

    messing with bins and interest rates

---
+++ dbt_snowflake/models/demos/bin_buckets/interest_rate_table.sql
@@ -0,0 +1,79 @@
+select 
+1::int as id,
+11::int as eir
+union all 
+select
+2::int as id,
+22::int as eir
+union all 
+select
+3::int as id,
+12::int as eir
+union all 
+select
+4::int as id,
+76::int as eir
+union all 
+select
+5::int as id,
+55::int as eir
+union all 
+select
+6::int as id,
+45::int as eir
+union all 
+select
+7::int as id,
+34::int as eir
+union all 
+select
+8::int as id,
+67::int as eir
+union all 
+select
+9::int as id,
+5::int as eir
+union all 
+select
+10::int as id,
+13::int as eir
+union all 
+select
+11::int as id,
+77::int as eir
+union all 
+select
+12::int as id,
+82::int as eir
+union all 
+select
+13::int as id,
+48::int as eir
+union all 
+select
+14::int as id,
+22::int as eir
+union all 
+select
+15::int as id,
+11::int as eir
+union all 
+select
+16::int as id,
+91::int as eir
+union all 
+select
+17::int as id,
+67::int as eir
+union all 
+select
+18::int as id,
+37::int as eir
+union all 
+select
+19::int as id,
+15::int as eir
+union all 
+select
+20::int as id,
+100::int as eir
