ee7c9f5471a4ab0c1a1511cd06b96a22243d1043
Pat Kearns <pat.kearns@dbtlabs.com>
2022-09-21 02:38:22 +0000

    union one time load and -1 key values

---
+++ dbt_snowflake/models/onsemi/corporation.sql
@@ -0,0 +1,45 @@
+-- incremental model to bring in:
+    -- one time load data
+    -- -1 for pk 
+    -- stg data 
+
+{{
+    config(
+        materialized='incremental',
+        unique_key="CORPORATION_KEY"
+        )
+}}
+
+with
+
+-- one time load with an added -1 for product key
+one_time_load as (
+    {{ dbt_utils.union_relations(
+    relations=[
+        ref('seed__corporation__one_time_load'),
+        ref('seed__negative_key'),
+        ],
+    ) }}
+
+),
+
+-- daily updated data
+stg_data as (
+    select * from {{ ref('seed__corporation__stg_data') }}
+    {%- if is_incremental() %}
+    where snapshot__date >= (select max(snapshot__date) from {{ this }})
+    {%- endif %}
+)
+
+select
+    -- using this to only select columns from the one_time_load_table
+    {{ dbt_utils.star(from=ref('seed__corporation__one_time_load')) }}
+from one_time_load
+    -- after the initial load, we will not need to run this again
+    {%- if is_incremental() %}
+    where snapshot__date >= (select max(snapshot__date) from {{ this }}) or snapshot_date is null
+    {%- endif %}
+
+union all
+
+select * from stg_data
