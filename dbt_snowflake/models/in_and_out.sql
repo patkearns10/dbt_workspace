26bea0396802b8387081102edf417685c4d1867f
Pat Kearns <pat.kearns@dbtlabs.com>
2023-03-02 02:18:45 +0000

    forgot to save files

--- dbt_snowflake/models/in_and_out.sql
+++ dbt_snowflake/models/in_and_out.sql
@@ -1,21 +1,27 @@
-{%- set catalog_name='default' %}
-{%- set intermediate_keywords=['int', 'staging', 'fixtures', 'flatfiles', 'dbt'] %}
-{%- if target.name == 'dev' %}
-    - {{ catalog_name }} 
-    {%- for word in intermediate_keywords %}
-        # is '{{ word }}' in '{{this.schema}}'
-        {%- if word in this.schema %}
-            # yes
-            {%- set catalog_name = 'intermediate' %}
-            -- {{ catalog_name }}                                  # within if block
-            {%- else %}
-            # no
-            -- {{ catalog_name }}                                  # within if block
-        {%- endif %}
-    --- {{ catalog_name }}                                         # within for loop
-    {%- endfor %}
-{%- else %}
-    # else
-{%- endif %}
+-- aka namespaces
+-- https://stackoverflow.com/questions/9486393/jinja2-change-the-value-of-a-variable-inside-a-loop
 
-select '{{ catalog_name }}' as column                              # I would expect this to return 'intermediate'
+
+{% set ns = namespace(catalog_name='') %}      
+{%- set intermediate_keywords=['int', 'staging', 'fixtures', 'flatfiles', 'dbt'] %}
+
+    {%- if target.name == 'dev' %}
+        - {{ ns.catalog_name }} 
+        {%- for word in intermediate_keywords %}
+            # is '{{ word }}' in '{{this.schema}}'
+            {%- if word in this.schema %}
+                # yes
+                {%- set ns.catalog_name = 'intermediate' %}
+                -- {{ ns.catalog_name }}                                  # within if block
+                {%- else %}
+                # no
+                -- {{ ns.catalog_name }}                                  # within if block
+            {%- endif %}
+        --- {{ ns.catalog_name }}                                         # within for loop
+        {%- endfor %}
+    {%- else %}
+        # else
+    {%- endif %}
+    --- {{ ns.catalog_name }} 
+
+select '{{ ns.catalog_name }}' as column                              # I would expect this to return 'intermediate'
