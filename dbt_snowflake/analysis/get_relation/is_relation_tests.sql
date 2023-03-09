a41ab936b42e465a28d7a450b1e9ecfee4a33674
Pat Kearns <pat.kearns@dbtlabs.com>
2023-03-09 02:52:10 +0000

    is_relation tests

--- dbt_snowflake/analysis/get_relation/is_relation_tests.sql
+++ dbt_snowflake/analysis/get_relation/is_relation_tests.sql
@@ -1,13 +1,23 @@
-{% set old_etl_relation=adapter.get_relation(
+{% set obj=adapter.get_relation(
       database=target.database,
       schema="DBT_PKEARNS",
       identifier="CUSTOMERS"
 ) -%}
 
 -- _is_relation tests
+{{ obj }}
+{{ obj is mapping }}
+{{ obj.get('metadata', {}).get('type', '').endswith('Relation') }}
 
-{{ old_etl_relation }}
-{{ old_etl_relation.get('metadata', {}) }}
-{{ old_etl_relation.get('metadata', {}).get('type', '') }}
-{{ old_etl_relation.get('metadata', {}).get('type', '').endswith('Relation') }}
-{{ old_etl_relation is mapping }}
+-- # we expect this to be true and true, so pass.
+    {% if not (obj is mapping and obj.get('metadata', {}).get('type', '').endswith('Relation')) %}
+        'pass'
+    {% else %}
+        'fail'
+    {% endif %}
+
+
+-- # storage
+{#
+{% do exceptions.raise_compiler_error("we got here: " ~ obj.get('metadata', {}).get('type', '')) %}
+#}
