1d3ec18655fd919a670a2d8331b2e633d1949e92
Pat Kearns <pat.kearns@dbtlabs.com>
2023-03-23 03:52:47 +0000

    comments

--- dbt_snowflake/macros/set_query_tag.sql
+++ dbt_snowflake/macros/set_query_tag.sql
@@ -4,14 +4,14 @@
 {% set new_query_tag = config.get('query_tag') %}
 
   {% if new_query_tag %}
-  
+    {# If they set a query tag in yml or config block #}
     {% set original_query_tag = get_current_query_tag() %}
     {{ log("Setting query_tag to '" ~ new_query_tag ~ "'. Will reset to '" ~ original_query_tag ~ "' after materialization.") }}
     {% do run_query("alter session set query_tag = '{}'".format(new_query_tag)) %}
     {{ return(original_query_tag)}}
   
   {% else %}
-  
+    {# else add this meta information by default #}
     {% set new_query_tag = '{{ "query_tag_type" : "DBT_MODEL", "database" : "{}", "model_name" : "{}", "package" : "{}", "schema" : "{}" , "target" : "{}","role" : "{}","dbt_inovacation_id" : "{}","run_started_at" : "{}"}}'.format(
         model.database,
         model.name,
