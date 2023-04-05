152bdcd4d4c278ed75e7185e4269f2de18d1492a
Pat Kearns <pat.kearns@dbtlabs.com>
2023-04-05 05:54:58 +0000

    commit process

--- dbt_snowflake/macros/run_query_log.sql
+++ dbt_snowflake/macros/run_query_log.sql
@@ -4,8 +4,27 @@
         {{ sql }}
     {% endcall %}
 
-    {% do log('DEBUG================================DEBUG', info=True) %}
-    {% do log("DEBUG - HOW MANY ITEMS in run_query(): " ~ load_result("run_query_statement").table | length, info=True) %}
-    {% do return(load_result("run_query_statement").table) %}
+
+    {% set results = load_result("run_query_statement").table %}
+    {% do log('DEBUG===============START=================DEBUG', info=True) %}
+    -----------------------------------
+    {% set ns = namespace(column_chars=0, row_chars=0, total_chars=0) -%}
+    {% for column in results.column_names -%}
+    {%- set ns.column_chars = ns.column_chars + (column | length) -%}
+    {% endfor %}
+    {% do log('columns = ' ~ results.columns | length, info=True) %}
+    {% do log('column_chars = ' ~ ns.column_chars, info=True) %}
+
+    {% for item in range(results.columns | length) -%}
+        {% for word in results.columns[item].values() | list -%}
+                {%- set ns.row_chars = ns.row_chars + (word | length) -%}
+        {% endfor -%}
+    {% endfor %}
+    {% do log('rows = ' ~ results.rows | length, info=True) %}
+    {% do log('row_chars = ' ~ ns.row_chars, info=True) %}
+    {% do log('total_chars = ' ~ ns.column_chars + ns.row_chars, info=True) %}
+
+    {% do log('DEBUG===============END=================DEBUG', info=True) %}
+    {% do return(results) %}
   
 {% endmacro %}
--- dbt_snowflake/models/run_query_return.sql
+++ dbt_snowflake/models/run_query_return.sql
@@ -1,5 +1,5 @@
 {% set payment_methods_query -%}
-    select distinct payment_method from {{ ref('stg_payments') }}
+    select status, payment_method from {{ ref('stg_payments') }}
     order by 1
 {% endset -%}
 
@@ -9,14 +9,31 @@
 {% endset -%}
 
 {% set payment_results = run_query(payment_methods_query) -%}
-    -- {{ payment_results | length }}
 {% set customer_results  = run_query(customers_query) -%}
-    -- {{ customer_results | length }}
 
 {%- if execute -%}
     {# Return the first column #}
         {% set payments_results_list = payment_results.columns[0].values() | list -%}
+-----------------------------------
+-----------------------------------
+{% set ns = namespace(column_chars=0, row_chars=0, total_chars=0) -%}
+{% for column in payment_results.column_names -%}
+{%- set ns.column_chars = ns.column_chars + (column | length) -%}
+{% endfor %}
+--- columns = {{ payment_results.columns | length }}
+--- column_chars = {{ ns.column_chars }}
+
+{% for item in range(payment_results.columns | length) -%}
+    {% for word in payment_results.columns[item].values() | list -%}
+            {%- set ns.row_chars = ns.row_chars + (word | length) -%}
+    {% endfor -%}
+{% endfor %}
+--- rows = {{ payment_results.rows | length }}
+--- row_chars = {{ ns.row_chars }}
+--- total_chars = {{ ns.column_chars + ns.row_chars }}
+
         {% set customers_results_list = customer_results.columns[0].values() | list -%}
+
     {%- else -%}
         {% set payments_results_list = [] -%}
         {% set customers_results_list = [] -%}
