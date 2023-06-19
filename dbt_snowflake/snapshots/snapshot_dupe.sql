d16fcd5d4f79fe3c6403c62d1168de1f73710ec7
Pat Kearns <pat.kearns@dbtlabs.com>
2023-06-19 04:26:47 +0000

    drop randy files

--- dbt_snowflake/snapshots/snapshot_dupe.sql
+++ dbt_snowflake/snapshots/snapshot_dupe.sql
@@ -7,10 +7,14 @@
       unique_key='some_value',
       strategy='check',
       check_cols=['id', 'some_value'],
+      enabled=False
     )
 }}
 
--- sample data
+-- this will fail because of dupes
+-- 03:54:07  Database Error in snapshot snapshot_dupe (snapshots/snapshot_dupe.sql)
+-- 100090 (42P18): Duplicate row detected during DML action
+
 select
  1 as id,
  3 as some_value,
