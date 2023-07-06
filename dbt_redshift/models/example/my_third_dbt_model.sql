80739b8917e4c6a94ec60c5b2c2bf6ff1ae49131
Pat Kearns <pat.kearns@dbtlabs.com>
2023-07-06 05:31:48 +0000

    add models

--- dbt_redshift/models/example/my_third_dbt_model.sql
+++ dbt_redshift/models/example/my_third_dbt_model.sql
@@ -1,7 +1,8 @@
 
 -- Use the `ref` function to select from other models
+
 {{ config(materialized='table') }}
 
 select *
-from {{ ref('my_first_dbt_model') }}
+from {{ ref('my_second_dbt_model') }}
 where id = 1
