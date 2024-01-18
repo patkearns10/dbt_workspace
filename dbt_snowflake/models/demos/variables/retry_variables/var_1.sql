cddc3db238fe2e57a9120d44a2d18bfdda1886f7
Pat Kearns <pat.kearns@dbtlabs.com>
2024-01-18 05:49:52 +0000

    fix error

--- dbt_snowflake/models/demos/variables/retry_variables/var_1.sql
+++ dbt_snowflake/models/demos/variables/retry_variables/var_1.sql
@@ -2,8 +2,8 @@
 -- {{ var('do_something') }}
 select 1 as col
     
+{% if var('do_something') == 'yessir' %}
     union all 
     
-{% if var('do_something') == 'yessir' %}
     select 2 as col
 {% endif %}
