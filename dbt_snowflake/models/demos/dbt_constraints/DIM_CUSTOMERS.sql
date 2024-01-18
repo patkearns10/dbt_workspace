4a34418630ceed12649528362d12b870fb825270
Pat Kearns <pat.kearns@dbtlabs.com>
2024-01-18 05:18:46 +0000

    indirect selection and constraints

---
+++ dbt_snowflake/models/demos/dbt_constraints/DIM_CUSTOMERS.sql
@@ -0,0 +1,21 @@
+select 
+100 as C_CUSTKEY,
+'yellow' as color
+
+union all
+
+select 
+200 as C_CUSTKEY,
+'red' as color
+
+union all
+
+select 
+300 as C_CUSTKEY,
+'blue' as color
+
+union all
+
+select 
+400 as C_CUSTKEY,
+'orange' as color
