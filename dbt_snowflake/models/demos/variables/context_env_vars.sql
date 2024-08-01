aa687cb71cad76faf92012b65af79763d0379089
Pat Kearns <pat.kearns@dbtlabs.com>
2024-08-01 23:43:44 +0000

    add defaults

--- dbt_snowflake/models/demos/variables/context_env_vars.sql
+++ dbt_snowflake/models/demos/variables/context_env_vars.sql
@@ -1,7 +1,7 @@
 select
 -- ex: dev, staging, ci, or prod
-'{{env_var('DBT_CLOUD_INVOCATION_CONTEXT')}}' as DBT_CLOUD_INVOCATION_CONTEXT,
+'{{env_var('DBT_CLOUD_INVOCATION_CONTEXT', "default")}}' as DBT_CLOUD_INVOCATION_CONTEXT,
 -- ex: "Development"
-'{{env_var('DBT_CLOUD_ENVIRONMENT_NAME')}}' as DBT_CLOUD_ENVIRONMENT_NAME,
+'{{env_var('DBT_CLOUD_ENVIRONMENT_NAME', "default")}}' as DBT_CLOUD_ENVIRONMENT_NAME,
 -- ex: dev, staging or prod
-'{{env_var('DBT_CLOUD_ENVIRONMENT_TYPE')}}' as DBT_CLOUD_ENVIRONMENT_TYPE
+'{{env_var('DBT_CLOUD_ENVIRONMENT_TYPE', "default")}}' as DBT_CLOUD_ENVIRONMENT_TYPE
