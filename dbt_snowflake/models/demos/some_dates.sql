fce4248104665046a0494c5e6d50ab7e5f016196
Pat Kearns <pat.kearns@dbtlabs.com>
2025-03-12 03:35:52 +0000

    add test for dates

---
+++ dbt_snowflake/models/demos/some_dates.sql
@@ -0,0 +1,28 @@
+with
+
+sample_data as (
+    select
+        1 as unique_id,
+        'good' as status,
+        date('2024-01-01') as start_date,
+        date('2024-01-02') as end_date,
+
+
+    union all
+
+    select
+        2 as unique_id,
+        'bad' as status,
+        date('2024-01-03') as start_date,
+        date('2024-01-02') as end_date,
+
+    union all
+
+    select
+        3 as unique_id,
+        'okay' as status,
+        date('2024-01-01') as start_date,
+        date('2024-01-01') as end_date,
+)
+
+select * from sample_data
