e93c80bf1d1e80a1ffe98d2c3da56b97b607ecda
Pat Kearns <pat.kearns@dbtlabs.com>
2025-03-12 03:37:00 +0000

    fix dates so they do not fail

--- dbt_snowflake/models/demos/some_dates.sql
+++ dbt_snowflake/models/demos/some_dates.sql
@@ -13,8 +13,8 @@ sample_data as (
     select
         2 as unique_id,
         'bad' as status,
-        date('2024-01-03') as start_date,
-        date('2024-01-02') as end_date,
+        date('2024-01-02') as start_date,
+        date('2024-01-03') as end_date,
 
     union all
 
@@ -22,7 +22,7 @@ sample_data as (
         3 as unique_id,
         'okay' as status,
         date('2024-01-01') as start_date,
-        date('2024-01-01') as end_date,
+        date('2024-01-04') as end_date,
 )
 
 select * from sample_data
