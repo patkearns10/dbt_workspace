b12bbac4c1a9cea346181fe44c8f9ad566aadfb3
Pat Kearns <pat.kearns@dbtlabs.com>
2025-08-29 00:40:21 +0000

    var to env_var, and do not count star for ephemerals

--- dbt_bigquery/macros/extract_run_results.sql
+++ dbt_bigquery/macros/extract_run_results.sql
@@ -7,7 +7,7 @@
     -- Loop through all nodes in manifest.json and extract all relevent metadata from models
     {% if execute %}
         {% for node in graph.nodes.values() %}
-            {% if node.name in parsed_results_dict and node.resource_type == "model" %}
+            {% if node.name in parsed_results_dict and node.resource_type == "model" and node.config.materialized != 'ephemeral' %}
                 {% set total_table_rows = get_row_count(node.database, node.schema, node.name, node.config.materialized) %}
                 {% set _ = parsed_results_dict[node.name].update({'identifier_keys': node.meta.identifier_keys}) %}
                 {% set _ = parsed_results_dict[node.name].update({'project': node.database}) %}
--- dbt_bigquery/macros/get_row_count.sql
+++ dbt_bigquery/macros/get_row_count.sql
@@ -2,21 +2,14 @@
 {% macro get_row_count(database_name, table_schema, table_name, materialized) %}
     {%- do print("get_row_count has started running") -%}
 
-    {% if materialized == 'ephemeral' %}
-        {{ return(0) }}
-    {% elif materialized == 'view' %}
-        -- pass variable to command to enable count(*) for view.
-        -- $ dbt build --vars '{"allow_count": "1"}'
-        {% if var('allow_count', 0) == 0 %}
-            {{ return(0) }}
-        {% else %}
-            {{ count_star(database_name, table_schema, table_name) }}
-        {% endif %}
-
+    {% if materialized == 'view' %}
+        -- set environment variable to enable or disable count(*).
+        {{ count_star(database_name, table_schema, table_name, env_var('DBT_COUNT_STAR', 'ALLOW')) }}
     {% else %}
 
     --NOTE: commenting this out because it doesnt work in my bigquery project
-    {# {% set regions = ["australia-southeast1", "us-east4", "europe-west2", "asia-southeast1"] %}
+{#  TODO: Brian add this back in
+    {% set regions = ["australia-southeast1", "us-east4", "europe-west2", "asia-southeast1"] %}
     {% for region in regions %}
         {% set table = database_name + ".region-" + region + ".INFORMATION_SCHEMA.TABLE_STORAGE" %}
         {% set query = "SELECT total_rows as row_count FROM `" + table + "` WHERE table_schema='" + table_schema + "' AND table_name='" + table_name + "'" %}
@@ -26,22 +19,29 @@
             {% set row_count = result[0]['row_count'] %}
             {{ return(row_count) }}
         {% else %}
-            -- Continue to the next region if no result is found
-            {% continue %}
+#}
+            -- set environment variable to enable or disable count(*).
+            {{ count_star(database_name, table_schema, table_name, env_var('DBT_COUNT_STAR', 'ALLOW')) }}
+{#  TODO: Brian add this back in
         {% endif %}
-    {% endfor %} #}
-    {{ count_star(database_name, table_schema, table_name) }}
-
+  
+    {% endfor %} 
+#}
     {% endif %}
 {% endmacro %}
 
 
-{% macro count_star(database_name, table_schema, table_name) %}
+{% macro count_star(database_name, table_schema, table_name, allow_flag) %}
     -- if not valid result is found in any region then use count(*)
-    {% set table_name_concat = database_name ~ '.' ~ table_schema ~ '.' ~ table_name %}
-    {% set query = "SELECT COUNT(*) as row_count FROM `" + table_name_concat + "`" %}
-    {% set result = run_query(query) %}
-    {% set row_count = result[0]['row_count'] %}
-    {%- do print("get_row_count has finished running") -%}
-    {{ return(row_count) }}
+    {% if allow_flag == 'ALLOW' %}
+        {% set table_name_concat = database_name ~ '.' ~ table_schema ~ '.' ~ table_name %}
+        {% set query = "SELECT COUNT(*) as row_count FROM `" + table_name_concat + "`" %}
+        {% set result = run_query(query) %}
+        {% set row_count = result[0]['row_count'] %}
+        {%- do print("get_row_count has finished running") -%}
+        {{ return(row_count) }}
+    -- env_var('DBT_COUNT_STAR') set to something other than 'ALLOW'
+    {% else %}
+        {{ return(0) }}
+    {% endif %}
 {% endmacro %}
