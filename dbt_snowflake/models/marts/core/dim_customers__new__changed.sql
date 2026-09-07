1e8658a8dc18722348d83cd9fa4fdb53dee2183c
Pat Kearns <pat.kearns@dbtlabs.com>
2026-09-07 00:10:11 +0000

    add +1 to food order

--- dbt_snowflake/models/marts/core/dim_customers__new__changed.sql
+++ dbt_snowflake/models/marts/core/dim_customers__new__changed.sql
@@ -21,7 +21,7 @@ order_summary as (
 
     select
         customer_id,
-        count(distinct orders.order_id)+1 as count_lifetime_orders,
+        count(distinct orders.order_id) as count_lifetime_orders,
         count(distinct orders.order_id) > 1 as is_repeat_buyer,
         min(orders.ordered_at) as first_ordered_at,
         max(orders.ordered_at) as last_ordered_at,
